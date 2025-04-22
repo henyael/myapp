3ade89d680f1e7a41c3dc173b3ce2cb28e67fd1c
henyael18 <42065139+henyael@users.noreply.github.com>
2025-04-22 15:40:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("comment number 2");
+Console.WriteLine("comment number 3");
+Console.WriteLine("comment number 4");
+Console.WriteLine("comment number 5");
