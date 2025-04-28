7fd157e574a46cf482f8b0d6aa26ff668603dc56
Hen <henyael91@gmail.com>
2025-04-28 23:28:37 +0300

    thank you Tamir

--- Program.cs
+++ Program.cs
@@ -1,53 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Hen{
 
-class Customer
+namespace Hen{
+public class Person
 {
     public string Name { get; set; }
-    public int TicketNumber { get; set; }
+    public string City { get; set; }
+    public int Age { get; set; }
 
- public Customer(string name, int ticketNumber){
-        Name=name;
-        TicketNumber=ticketNumber;
- }   
+public Person(string name, string city, int age)
+        {
+            Name = name;
+            City = city;
+            Age = age;
+        }
 }
 
-class Program
+public class CheckingSocialCircle{
+    public List<Person>People;
+
+public void Init(Person[] people){
+    this.people=people.ToList();
+}
+bool AreInSameSocialCircle(Person personA, Person personB)
 {
-static public Queue<Customer> Customerqueue=new Queue<Customer>();
-
-static public void Search(string a){
-bool found=false;
-foreach(Customer item in Customerqueue){
-if(item.Name==a){
-Console.WriteLine($"Yey, there is a match in the name, the ticket num is{item.TicketNumber}");
-found=true;
-break;
+if( personA==personB){
+    return true;
 }
 
+Queue<Person> queue=new Queue<Person>();
+HashSet<Person> visited=new HashSet<Person>();
+queue.Enqueue(personA);
+visited.Add(personA);
+while(queue.Count>0)
+var current=queue.Dequeue();
+{
+foreach(var item in people)
+{
+    if(!visited.Contain(item)&&(item.City==current.City||item.Age==current.Age))
+    {
+        if(personB==item)
+        {
+         return true;
+         }
+        queue.Enqueue(item);
+        visited.Add(item);
+    }
 }
-if(found==false){
-    Console.WriteLine("Sorry, we didnt find any match");
 }
-}
-static public void Main(string[]args){
-Customer customer1=new Customer("Hen",1);
-Customer customer2=new Customer("Gal",2);
-Customer customer3=new Customer("Avi",3);
-Customer customer4=new Customer("Dan",4);
-Customer customer5=new Customer("Ron",5);
-Customerqueue.Enqueue(customer1);
-Customerqueue.Enqueue(customer2);
-Customerqueue.Enqueue(customer3);
-Customerqueue.Enqueue(customer4);
-Customerqueue.Enqueue(customer5);
+    return false;
 
-Console.WriteLine("Hey user,pls enter a name");
-string name=Console.ReadLine();
-Search(name);
-
 }
+}
+
+class Program{
+
+static public void Main (string[] args)
+{
+Person Person1=new Person("Dan","Tel Aviv",25);
+Person Person2=new Person("Yossi","Tel Aviv",30);
+Person Person3=new Person("Beni","Haifa",25);
+Person Person4=new Person("Omer","Jerusalem",40);
+
+People.Add(Person1);
+People.Add(Person2);
+People.Add(Person3);
+People.Add(Person4);
+
+CheckingSocialCircle.AreInSameSocialCircle(people);
+}
+
 }
 }
